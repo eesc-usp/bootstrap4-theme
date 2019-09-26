48796b1d443b9667d0a31baeea54cf360a478751
Masaki Kawabata Neto <kawabata@usp.br>
2019-09-26 10:52:15 -0300

    implementacao incial

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# template-sistemas
+# Template-sistemas
 Template básico para sistemas da unidade
+
+## Instalação
+
+Coloque no seu composer.json 
+```
+    "repositories": [
+        {
+            "type": "composer",
+            "url": "https://asset-packagist.org"
+        }
+    ],
+    "extra": {
+        "installer-types": [
+            "bower-asset",
+            "npm-asset"
+        ],
+        "installer-paths": {
+            "www/assets/{$name}/": [
+                "type:bower-asset",
+                "type:npm-asset"
+            ]
+        }
+    },
+```
+Depois 
+
+```
+Composer require eesc/bootstrap4-theme
+```
+
+
+## Configuração
+
+Este template copia os assets para a pasta www/ que é a pasta publica que deve ser servida pelo Apache.
---
+++ src/Theme.php
@@ -0,0 +1,47 @@
+<?php
+namespace EESC;
+
+class Theme
+{
+    public static function renderNavbar($navbar)
+    {
+        $tpl = new \raelgc\view\Template('../template/navbar.html');
+        foreach ($navbar['navbar_left'] as $nb) {
+            $tpl->nb_url = $nb['url'];
+            $tpl->nb_text = $nb['text'];
+            $tpl->nb_title = $nb['title'] ?: '';
+            $tpl->block('NAVBAR_ITEM');
+        }
+
+        foreach ($navbar['navbar_right'] as $nb) {
+            $tpl->nb_url = $nb['url'];
+            $tpl->nb_text = $nb['text'];
+            $tpl->nb_title = $nb['title'] ?: '';
+            $tpl->block('NAVBAR_ITEM_RIGHT');
+        }
+
+        return $tpl->parse();
+    }
+
+    public static function renderUserbar($userbar)
+    {
+        $tpl = new \raelgc\view\Template('../template/userbar.html');
+
+        if ($userbar['login']) {
+            $tpl->user = $userbar['username'];
+            if ($userbar['role']) {
+                $tpl->role = $userbar['role'];
+                $tpl->block('ROLE');
+            }
+            $tpl->logout_url = $userbar['logout_url'];
+            $tpl->block('LOGGED_IN');
+
+        } else {
+            $tpl->login_url = $userbar['login_url'];
+            $tpl->block('LOGGED_OUT');
+
+        };
+
+        return $tpl->parse();
+    }
+}
